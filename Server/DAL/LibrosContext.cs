cfee1754dc96c770d966ffcdad9ad5c70ca46a3f
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-20 21:29:02 -0400

    Agregando Contexto y la ConnectionString.

---
+++ Server/DAL/LibrosContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal.Shared.Models;
+
+namespace ProyectoFinal.Server.DAL;
+
+public class LibrosContext : DbContext
+{
+	public LibrosContext(DbContextOptions<LibrosContext> options)
+		:base(options) { }
+
+	public DbSet<Libros> Libros { get; set; }
+}
+
